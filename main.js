8c4c3ccd1c3354f4482affb744ff0e2dcd9ae678
Elyorxon <elyor99-99@mail.ru>
2023-11-27 17:59:01 +0500

    js2

---
+++ main.js
@@ -0,0 +1,35 @@
+//console.log(10); -это способ ввода информации в консоль браузера
+//console.log('Elyorxon'); строчный тип данных
+//console.log(24); числовой тип данный
+//console.log(true); логический тип данных true
+//console.log(false); логический тип данных false
+// var boss = 'Elyorxon';
+// var age= 23;
+// boss = 'JS'
+// console.log('My name is ' + boss + ' I am ' + age + ' years old');
+// var number = 20
+// var myWord = 'My number ' + number
+// console.log(myWord);
+
+// prompt() - создаёт окошко для ввода данных
+// prompt() - создаёт окошко для ввода данных числовой
+// alert()- создаёт нам модальное окно
+// 
+
+// // alert(str);
+// console.log('10 + 5 = ' + (10 + 5));
+// console.log('10 - 5 = ' + (10 - 5));
+// console.log('10 * 5 = ' + (10 * 5));
+// console.log('10 / 5 = ' + (10 / 5));
+
+var x = +prompt('Введите первое число');
+console.log(x);
+var y = +prompt('Введите второе число');
+console.log(y);
+var z = +prompt('Введите третье число');
+console.log(z);
+console.log('Среднее арифметическое Ваших чисел = ' + ((x + y + z)/3));
+alert('Среднее арифметическое Ваших чисел = ' + ((x + y + z)/3));
+
+
+
